729edcc7b9eadaa69f41ed5d603231d7f70b72da
lkmSasanga <lkmsasanga@gmail.com>
2020-11-12 16:18:40 +0530

    js es6 object property shorthand & destructuring

---
+++ playground/5-es6-objects.js
@@ -0,0 +1,36 @@
+const name = 'Malindu'
+const userAge = 23
+
+const user = {
+    name,           //shorthand
+    age: userAge,
+    location: 'Galle'
+}
+
+console.log(user)
+
+
+//destructuring
+
+const product = {
+    label: 'Red Notebook',
+    price: 3,
+    stock: 201,
+    salePrice: undefined
+}
+
+// const label = product.label
+// const stock = product.stock
+
+// const {label: productLabel, stock, rating = 5} = product
+
+// console.log(productLabel)
+// console.log(stock)
+// console.log(rating)
+
+
+const transaction = (type, {label, stock}) => {
+    console.log(type, label, stock)
+}
+
+transaction('order', product)
