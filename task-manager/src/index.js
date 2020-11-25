b655348011bc64626d25b9346db6427b3a862c53
lkmSasanga <lkmsasanga@gmail.com>
2020-11-25 19:50:01 +0530

    user data managing permissions

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -28,14 +28,28 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
 
-const jwt = require('jsonwebtoken')
+// const jwt = require('jsonwebtoken')
+//
+// const myFunction = async () => {
+//     const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcourse', { expiresIn: '7 days' })
+//     console.log(token)
+//
+//     const data = jwt.verify(token, 'thisismynewcourse')
+//     console.log(data)
+// }
+//
+// myFunction()
 
-const myFunction = async () => {
-    const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcourse', { expiresIn: '7 days' })
-    console.log(token)
+const Task = require('./models/task')
+const User = require('./models/user')
 
-    const data = jwt.verify(token, 'thisismynewcourse')
-    console.log(data)
+const main = async () => {
+//     const task = await Task.findById('5fbe514e56ff3f1384254b69')
+//     await task.populate('owner').execPopulate()
+//     console.log(task.owner)
+    const user = await User.findById('5fbe501b0bf65b4cc0ac10af')
+    await user.populate('tasks').execPopulate()
+    console.log(user.tasks)
 }
 
-myFunction()
+main()
--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const Task = require('../models/task')
+const auth = require('../middleware/auth')
 const router = new express.Router()
 
 
-router.post('/tasks', async (req, res) => {
-    const task = new Task(req.body)
-
+router.post('/tasks', auth, async (req, res) => {
+    const task = new Task({
+        ...req.body,  // spread operator
+        owner: req.user._id
+    })
     try {
         await task.save()
         res.status(201).send(task)
@@ -14,21 +17,22 @@ router.post('/tasks', async (req, res) => {
     }
 })
 
-router.get('/tasks', async (req, res) => {
+router.get('/tasks', auth, async (req, res) => {
     try {
-        const tasks = await Task.find({})
-        res.send(tasks)
+        // const tasks = await Task.find({ owner: req.user._id })
+        await req.user.populate('tasks').execPopulate()
+        res.send(req.user.tasks)
     } catch (e) {
         res.status(500).send()
     }
 })
 
 
-router.get('/tasks/:id', async (req, res) => {
+router.get('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id
 
     try {
-        const task = await Task.findById(_id)
+        const task = await Task.findOne({_id, owner: req.user._id})
 
         if(!task) {
             return res.status(404).send()
@@ -39,7 +43,7 @@ router.get('/tasks/:id', async (req, res) => {
     }
 })
 
-router.patch('/tasks/:id', async (req, res) => {
+router.patch('/tasks/:id', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowUpdates = ['description', 'completed']
     const isValidOperation = updates.every((update) => allowUpdates.includes(update))
@@ -49,16 +53,13 @@ router.patch('/tasks/:id', async (req, res) => {
     }
 
     try {
-        const task = await Task.findById(req.params.id)
-
-        updates.forEach((update)=> task[update] = req.body[update])
-
-        await task.save()
-        // const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        const task = await Task.findOne({_id: req.params.id, owner: req.user._id})
 
         if(!task) {
             return res.status((404)).send()
         }
+        updates.forEach((update)=> task[update] = req.body[update])
+        await task.save()
 
         res.send(task)
     } catch (e) {
@@ -66,10 +67,9 @@ router.patch('/tasks/:id', async (req, res) => {
     }
 })
 
-router.delete('/tasks/:id', async (req, res) => {
+router.delete('/tasks/:id', auth, async (req, res) => {
     try{
-        const task = await Task.findByIdAndDelete(req.params.id)
-
+        const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id })
         if(!task) {
             return res.status(404).send()
         }
