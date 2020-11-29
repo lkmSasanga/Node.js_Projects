2368198954294299441c418ae3fba9c3b47319db
lkmSasanga <lkmsasanga@gmail.com>
2020-11-29 10:13:55 +0530

    tested

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -3,6 +3,7 @@ const multer = require('multer')
 const sharp = require('sharp')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
+const { sendWelcomeEmail, sendCancelationEmail } = require('../emails/account')
 const router = new express.Router()
 
 // create user
@@ -11,6 +12,7 @@ router.post('/users', async (req, res) => {
 
     try {
         await user.save()
+        sendWelcomeEmail(user.email, user.name)
         const token = await user.generateAuthToken()
         res.status(201).send({ user, token })
     } catch (e) {
@@ -55,7 +57,6 @@ router.get('/users/me', auth, async (req, res) => {
     res.send(req.user)
 })
 
-// updating an existing user
 router.patch('/users/me', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['name', 'email', 'password', 'age']
@@ -77,6 +78,7 @@ router.patch('/users/me', auth, async (req, res) => {
 router.delete('/users/me', auth, async (req, res) => {
     try {
         await req.user.remove()
+        sendCancelationEmail(req.user.email, req.user.name)
         res.send(req.user)
     } catch (e) {
         res.status(500).send()
